58f5e02f0a3772d7ebf04969f79321e22f06dfc8
tomixy <tomix05may@gmail.com>
2021-12-08 05:56:33 +0900

    Fixed display corruption on iOS

--- resources/views/components/front/contact.blade.php
+++ resources/views/components/front/contact.blade.php
@@ -1,37 +1,37 @@
 <div class="contact">
-    <div class="contact_title">/*CONTACT*/</div>
+    <div class="contact_title">/*contact*/</div>
     <form action="{{ route('front.contact.confirm') }}" method="POST" target="_blank" rel="noopener noreferrer">
     @csrf
         <p>
-            <label for="name">NAME</label><br>
+            <label for="name">name</label><br>
             <input type="text" name="name" size="40" value="{{ old('name') }}">
             @if ($errors->has('name'))
             <p>{{ $errors->first('name') }}</p>
             @endif
         </p>
         <p>
-            <label for="email">MAIL-ADRESS</label><br>
+            <label for="email">mail-address</label><br>
             <input type="email" name="email" size="40" value="{{ old('email') }}">
             @if ($errors->has('email'))
             <p>{{ $errors->first('email') }}</p>
             @endif
         </p>
         <p>
-            <label for="title">TITLE</label><br>
+            <label for="title">title</label><br>
             <input type="text" name="title" size="40" value="{{ old('title') }}">
             @if ($errors->has('title'))
             <p>{{ $errors->first('title') }}</p>
             @endif
         </p>
         <p>
-            <label for="contents">CONTENTS</label><br>
+            <label for="contents">contents</label><br>
             <textarea name="contents" cols="40" rows="10" value="{{ old('contents') }}"></textarea>
             @if ($errors->has('contents'))
             <p>{{ $errors->first('contents') }}</p>
             @endif
         </p>
         <p>
-            <input type="submit" value="CONFIRM">
+            <input type="submit" value="confirm">
         </p>
     </form>
 </div>
